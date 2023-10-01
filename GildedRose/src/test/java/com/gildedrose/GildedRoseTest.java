d289b65d4379525033f6897272a5e6e0de3a498b
Moran <moran38villard@gmail.com>
2023-10-01 23:23:58 +0200

    Commit 2 Test: 100% coverage

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -47,6 +47,61 @@ class GildedRoseTest {
     assertEquals(0, element.quality);
   }
 
+  @Test
+  void testQualityNeverMoreThan50() {
+    Item element = new Item("Aged Brie", 10, 50);
+    GildedRose app = new GildedRose(new Item[]{element});
+    app.updateQuality();
+    assertEquals(50, element.quality);
+  }
+
+  @Test
+  void testSulfurasNeverChanges() {
+    Item element = new Item("Sulfuras, Hand of Ragnaros", 10, 80);
+    GildedRose app = new GildedRose(new Item[]{element});
+    app.updateQuality();
+    assertEquals(80, element.quality);
+    assertEquals(10, element.sellIn);
+  }
+
+  @Test
+  void testBackstagePassesQualityIncreaseBy2() {
+    Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20);
+    GildedRose app = new GildedRose(new Item[]{element});
+    app.updateQuality();
+    assertEquals(22, element.quality);
+  }
+
+  @Test
+  void testBackstagePassesQualityIncreaseBy3() {
+    Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20);
+    GildedRose app = new GildedRose(new Item[]{element});
+    app.updateQuality();
+    assertEquals(23, element.quality);
+  }
+
+  @Test
+  void testBackstagePassesQualityDropsTo0AfterConcert() {
+    Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20);
+    GildedRose app = new GildedRose(new Item[]{element});
+    app.updateQuality();
+    assertEquals(0, element.quality);
+  }
+
+  @Test
+  void testAgedBrieQualityIncreaseAfterSellIn() {
+    Item element = new Item("Aged Brie", -1, 20);
+    GildedRose app = new GildedRose(new Item[]{element});
+    app.updateQuality();
+    assertEquals(22, element.quality); // Quality should increase by 2 after sell-by date
+  }
+
+  @Test
+  void testItemToString() {
+    Item element = new Item("Aged Brie", 10, 20);
+    String expectedString = "Aged Brie, 10, 20";
+    assertEquals(expectedString, element.toString());
+  }
 
 }
 
