c5f291e4aaf6293137fced1a7a4f0e2d5ac1325f
Moran <moran38villard@gmail.com>
2023-10-01 22:53:43 +0200

    Start of project 52% coverage

---
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,20 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GildedRoseTest {
+
+  @Test
+  @DisplayName("Test that the name is unchanged")
+  void testName() {
+    Item element = new Item("foo", 0, 0);
+    GildedRose app = new GildedRose(new Item[]{element});
+    app.updateQuality();
+    assertEquals("foo", element.name, "the name changed");
+  }
+  
+}
+
+
+
