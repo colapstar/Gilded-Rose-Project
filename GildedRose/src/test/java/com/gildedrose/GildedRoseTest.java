73767fcf1c48a89b29ca10b28cef7f16bac281b6
Moran <moran38villard@gmail.com>
2023-10-01 23:04:32 +0200

    Commit 1 Test: 72% coverage

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -13,7 +13,41 @@ class GildedRoseTest {
     app.updateQuality();
     assertEquals("foo", element.name, "the name changed");
   }
-  
+
+  @Test
+  void testQualityDecreaseForNormalItem() {
+    Item element = new Item("normal", 10, 20);
+    GildedRose app = new GildedRose(new Item[]{element});
+    app.updateQuality();
+    assertEquals(19, element.quality);
+    assertEquals(9, element.sellIn);
+  }
+
+  @Test
+  void testQualityDecreaseTwiceAfterSellIn() {
+    Item element = new Item("normal", 0, 20);
+    GildedRose app = new GildedRose(new Item[]{element});
+    app.updateQuality();
+    assertEquals(18, element.quality);
+  }
+
+  @Test
+  void testAgedBrieQualityIncrease() {
+    Item element = new Item("Aged Brie", 10, 20);
+    GildedRose app = new GildedRose(new Item[]{element});
+    app.updateQuality();
+    assertEquals(21, element.quality);
+  }
+
+  @Test
+  void testQualityNeverNegative() {
+    Item element = new Item("normal", 10, 0);
+    GildedRose app = new GildedRose(new Item[]{element});
+    app.updateQuality();
+    assertEquals(0, element.quality);
+  }
+
+
 }
 
 
